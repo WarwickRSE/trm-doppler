34db34f86981e8a946b509772aa2d5607d3b90b1
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2026-02-02 12:27:22 +0000

    Fix Print

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ if 'TRM_SOFTWARE' in os.environ:
     library_dirs.append(os.path.join(os.environ['TRM_SOFTWARE'], 'lib'))
     include_dirs.append(os.path.join(os.environ['TRM_SOFTWARE'], 'include'))
 else:
-    print >>sys.stderr, \
-        "Environment variable TRM_SOFTWARE pointing to location of shareable libraries and includes not defined!"
+    print( \
+        "Environment variable TRM_SOFTWARE pointing to location of shareable libraries and includes not defined!", file=sys.stderr)
 
 include_dirs.append(numpy.get_include())
 
